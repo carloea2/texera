f94fe2ae635b86c7d851f3e445f8099604a111b9
mengw15 <125719918+mengw15@users.noreply.github.com>
2025-02-18 03:39:23 -0800

    update pythonOperatorDescriptor

--- core/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/PythonOperatorDescriptor.scala
+++ core/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/PythonOperatorDescriptor.scala
@@ -5,11 +5,11 @@ import edu.uci.ics.amber.core.tuple.Schema
 import edu.uci.ics.amber.core.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
 import edu.uci.ics.amber.core.workflow.{PhysicalOp, PortIdentity, SchemaPropagationFunc}
 
-trait PythonOperatorDescriptor extends LogicalOp {
+trait PythonOperatorDescriptor extends LogicalOp with ManualLocationConfiguration{
   override def getPhysicalOp(
-      workflowId: WorkflowIdentity,
-      executionId: ExecutionIdentity
-  ): PhysicalOp = {
+                              workflowId: WorkflowIdentity,
+                              executionId: ExecutionIdentity
+                            ): PhysicalOp = {
     val physicalOp = if (asSource()) {
       PhysicalOp.sourcePhysicalOp(
         workflowId,
@@ -26,11 +26,13 @@ trait PythonOperatorDescriptor extends LogicalOp {
       )
     }
 
-    physicalOp
+    val baseOp = physicalOp
       .withInputPorts(operatorInfo.inputPorts)
       .withOutputPorts(operatorInfo.outputPorts)
       .withParallelizable(parallelizable())
       .withPropagateSchema(SchemaPropagationFunc(inputSchemas => getOutputSchemas(inputSchemas)))
+
+    applyManualLocation(baseOp)
   }
 
   def parallelizable(): Boolean = false
