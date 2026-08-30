3474db3d458b4f56597a33adcdb03a22c58c63f8
carloea2 <carloea2@uci.edu>
2026-08-30 14:02:15 -0600

    fix(amber): return 404 for unavailable public workflow

--- amber/src/main/scala/org/apache/texera/web/resource/dashboard/user/workflow/WorkflowResource.scala
+++ amber/src/main/scala/org/apache/texera/web/resource/dashboard/user/workflow/WorkflowResource.scala
@@ -840,6 +840,8 @@ class WorkflowResource extends LazyLogging {
       .where(WORKFLOW.WID.eq(wid))
       .and(WORKFLOW.IS_PUBLIC.isTrue)
       .fetchOne()
+    if (workflow == null)
+      throw new NotFoundException(s"Public workflow with id $wid not found")
     WorkflowWithPrivilege(
       workflow.getName,
       workflow.getDescription,
